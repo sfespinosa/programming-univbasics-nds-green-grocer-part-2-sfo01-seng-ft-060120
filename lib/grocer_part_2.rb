7eec66e93f4a2298d9ea1ba22ff028b32535c422
Scott Espinosa <scottjames.espinosa@gmail.com>
2020-05-26 03:57:45 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -36,6 +36,7 @@ def apply_coupons(cart, coupons)
 end
 
 def apply_clearance(cart)
+<<<<<<< HEAD
   counter = 0 
   while counter < cart.length 
     if cart[counter][:clearance]
@@ -44,14 +45,38 @@ def apply_clearance(cart)
     counter += 1
   end 
   cart
+=======
+  clearanced_cart = []
+  cart.each do |item|
+    item.each do |item_type, item_detail|
+      if item_detail == true 
+        item[:price] *= 0.8
+        clearanced_cart.push(item)
+      else 
+        clearanced_cart.push(item)
+      end
+    end 
+  end 
+  clearanced_cart
+>>>>>>> 4ba54bef8308cc7cc8e4373d305589a6cfc99f0b
 end
 
 def checkout(cart, coupons)
   total = 0
+<<<<<<< HEAD
   consolidated_cart = consolidate_cart(cart)
   couponed_cart = apply_coupons(consolidated_cart, coupons)
   clearanced_cart = apply_clearance(couponed_cart)
   clearanced_cart.each do |cart_item|
+=======
+  binding.pry
+  consolidate_cart(cart)
+  apply_coupons(cart, coupons)
+  apply_clearance(cart)
+  binding.pry
+  cart.each do |cart_item|
+    binding.pry
+>>>>>>> 4ba54bef8308cc7cc8e4373d305589a6cfc99f0b
     total += cart_item[:price] * cart_item[:count]
   end 
   if total > 100 
--- lib/part_1_solution.rb
+++ lib/part_1_solution.rb
@@ -12,6 +12,7 @@ end
 
 def consolidate_cart(cart)
   new_cart = []
+<<<<<<< HEAD
   counter = 0 
   while counter < cart.length 
     new_cart_item = find_item_by_name_in_collection(cart[counter][:item], new_cart)
@@ -27,6 +28,21 @@ def consolidate_cart(cart)
       new_cart << new_cart_item
     end
     counter += 1 
+=======
+  cart.each do |cart_item|
+    new_cart_item = find_item_by_name_in_collection(cart_item[:item], new_cart)
+      if new_cart_item != nil 
+        new_cart_item[:count] += 1 
+      else 
+        new_cart_item = {
+          :item => cart_item[:item],
+          :price => cart_item[:price],
+          :clearance => cart_item[:clearance],
+          :count => 1 
+        }
+        new_cart << new_cart_item
+      end 
+>>>>>>> 4ba54bef8308cc7cc8e4373d305589a6cfc99f0b
   end 
   new_cart
 end
